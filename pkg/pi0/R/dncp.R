1c828fa14dd0c13e02bded286b4505002650f859
longor <longor@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-23 18:12:37 +0000

    dncp generic function added for computing the pdf of ncp from ncpest classes (parncp, nparncp, <sparncp is commented out>).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pi0@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/pi0/R/dncp.R
@@ -0,0 +1,19 @@
+dncp=function(obj, ...) UseMethod("dncp")
+
+dncp.parncp=function(obj, ...)
+{
+    ans=function(x)dnorm(x,obj$mu.ncp,obj$sd.ncp)
+    ans
+}
+
+dncp.nparncp=function(obj, ...)
+    d.ncp = function(xx) {
+        xx = outer(xx, obj$all.mus, "-")
+        xx = sweep(xx, 2, obj$all.sigs, "/")
+        d = sweep(dnorm(xx), 2, obj$all.sigs, "/")
+        drop(d %*% ob$beta)
+    }
+    d.ncp
+}
+
+#dncp.sparncp=function(obj,...) dncp.nparncp(obj,...)
