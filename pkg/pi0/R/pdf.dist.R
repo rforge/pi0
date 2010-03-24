39c522b2052bdd984e4e0182f335bd22bb5b947c
longor <longor@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-24 05:19:47 +0000

    pdf.dist.R now returns "named" results
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pi0@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pi0/R/pdf.dist.R
+++ pkg/pi0/R/pdf.dist.R
@@ -4,11 +4,11 @@ pdf.dist=function(f1, f2, method=c('Hellinger','abdif'))
     if(method=='abdif') {
         f=function(x)abs(f1(x)-f2(x))
         ans=try( integrate(f, -Inf, Inf)$value )
-        return( if(is.numeric(ans)) ans else NA_real_ )
+        return( c(abdif=if(is.numeric(ans)) ans else NA_real_ ))
     }
     if(method=='Hellinger') {
         f=function(x) (sqrt(f1(x))-sqrt(f2(x)))^2
         ans=try( integrate(f, -Inf, Inf)$value )
-         return( if(is.numeric(ans)) sqrt(.5*ans) else NA_real_)
+         return( c(Hellinger=if(is.numeric(ans)) sqrt(.5*ans) else NA_real_))
    }
 }
