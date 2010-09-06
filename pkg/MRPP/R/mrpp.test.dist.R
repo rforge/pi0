13ebc7ba6a4ac2c64d0f38118b054d8c38ae1c4c
longor <longor@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-09-06 08:22:08 +0000

    add PACKAGE argument to .C calls
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pi0@42 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/MRPP/R/mrpp.test.dist.R
+++ pkg/MRPP/R/mrpp.test.dist.R
@@ -12,7 +12,7 @@ function(y, trt, B=choose(length(trt),table(trt)[1]), perm.mat, wtmethod=0) ## t
                              'and permutation matrix', deparse(substitute(perm.mat)))
     B=ncol(perm.mat)
     cperm.mat=apply(perm.mat, 2, function(kk)(1:N)[-kk])
-    stats=.C('mrppstats',y,perm.mat,cperm.mat,nrow(perm.mat),B,N,as.integer(wtmethod[1]), ans=double(B))$ans
+    stats=.C('mrppstats',y,perm.mat,cperm.mat,nrow(perm.mat),B,N,as.integer(wtmethod[1]), ans=double(B),PACKAGE='MRPP')$ans
     ans=list(statistic=c("MRPP statistic"=stats[1]), all.statistics=stats, 
              p.value=mean(stats[1]>=stats), parameter=c("number of permutations"=B, 'weight method'=wtmethod[1]),
              data.name=dname, .Random.seed=attr(perm.mat,'.Random.seed'),
