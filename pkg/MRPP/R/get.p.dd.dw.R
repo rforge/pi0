38c051c184a1e477a59b3f23634b66e0eb89a5d7
longor <longor@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-09-14 18:16:55 +0000

    removed the distObj argument from get.p.dd.dw
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pi0@97 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/MRPP/R/get.p.dd.dw.R
+++ pkg/MRPP/R/get.p.dd.dw.R
@@ -1,6 +1,6 @@
 get.p.dd.dw <-
 function(y, perm.mat, r=1:ncol(as.matrix(y)), #test=F, 
-                        distObj=dist(y), 
+                        #distObj=dist(y), 
                         cperm.mat, wtmethod=integer(1)
                     )      
 ## b=permutation index; r=dimension index; 
@@ -14,7 +14,9 @@ function(y, perm.mat, r=1:ncol(as.matrix(y)), #test=F,
 
     contrast.mat=matrix(0,choose(N,2),N); k=1
     for(i in 1:(N-1))for(j in (i+1):N){contrast.mat[k,i]=1;contrast.mat[k,j]=-1;k=k+1}
-    all.ddelta.dw=abs(contrast.mat%*%y)^2/pmax(1e-8,distObj)/2
+    diffy=contrast.mat%*%y; diffy2=diffy*diffy
+    all.ddelta.dw=diffy2/pmax(1e-8,sqrt(rowSums(diffy2)))/2
+#    all.ddelta.dw=abs(contrast.mat%*%y)^2/pmax(1e-8,dist(y))/2  ## this works
 
     for(r.i in seq(along=r)){
         dz.dw=.C('mrppstats',all.ddelta.dw[,r.i],perm.mat,cperm.mat,nrow(perm.mat),B,N,as.integer(wtmethod[1]),ans=double(B),PACKAGE='MRPP',DUP=FALSE)$ans
