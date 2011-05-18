af5105aaa32f00c98f090e0dfce98085bc18768a
longor <longor@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-18 07:11:55 +0000

    corrected the return value from discTMix
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pi0@164 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pi0/R/discTMix.R
+++ pkg/pi0/R/discTMix.R
@@ -157,6 +157,7 @@ discTMix= function(tstat, n1=10, n2=n1, nq, p0, p1, D, delta, paired=FALSE,
     ans=list(p0.est=p0.est, p0.raw=p0, p1=p1, D = delta/sf, delta = delta, 
          threshold.delta=threshold.delta, AIC=AIC, opt=oo, data=list(tstat=tstat, df=n1+n2-2))
     class(ans)='discTMix'
+    ans
 }
 
 
