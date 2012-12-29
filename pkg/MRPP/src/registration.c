fb1b5e9344e1c20a2f7cef1107902c7394370d63
longor <longor@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-12-29 22:18:35 +0000

    debugged registration information
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pi0@294 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/MRPP/src/registration.c
+++ pkg/MRPP/src/registration.c
@@ -1,15 +1,16 @@
 #include <R.h>
 #include <Rinternals.h>
 #include <Rdefines.h>
+#include <R_ext/Rdynload.h>
 
 extern SEXP radixSort_prealloc(SEXP, SEXP); 
 extern SEXP mrppstats(SEXP , SEXP , SEXP ); 
-extern SEXP FR2permvec(const SEXP , const SEXP)
+extern SEXP FR2permvec(const SEXP , const SEXP);
 
 R_CallMethodDef callMethods[]  = {
        {"radixSort_prealloc", (DL_FUNC) &radixSort_prealloc, 2},
        {"mrppstats", (DL_FUNC) &mrppstats, 3},
-       {"FR2permvec", (DL_FUNC) &FR2permvec, 2},
+       {"C_FR2permvec", (DL_FUNC) &FR2permvec, 2},
        {NULL, NULL, 0}
 };
 
